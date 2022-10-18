3fd816a28af340a601e2fdeb660a79887a1499cd
Lauren Metz <Laurenmetz15@gmail.com>
2022-10-18 18:19:05 -0400

    commit tree 4bdfc8aff272cc0c5e8c784403b4ff82eb7f5d41

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewPlantForm() {
+function NewPlantForm(plants, setPlants) {
+
+  const [plantName, setPlantName] = useState("");
+  const [plantImage, setPlantImage] = useState("");
+  const [plantPrice, setPlantPrice] = useState(0);
+
+
+  function addPlant(e) {
+    e.preventDefualt();
+    const newPlant = {};
+    newPlant.name = plantName;
+    newPlant.image = plantImage;
+    newPlant.price = plantPrice;
+    console.log(newPlant);
+    //setPlants(...plants, newPlant);
+
+  }
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={addPlant}>
+        <input onChange={(e) => setPlantName(e.target.value)} type="text" name="name" placeholder="Plant name" />
+        <input onChange={(e) => setPlantImage(e.target.value)}type="text" name="image" placeholder="Image URL" />
+        <input onChange={(e) => setPlantPrice(e.target.value)}type="number" name="price" step="0.01" placeholder="Price" />
         <button type="submit">Add Plant</button>
       </form>
     </div>
